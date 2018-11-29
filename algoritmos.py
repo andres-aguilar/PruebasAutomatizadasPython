b504d7c127addb1e92ea049e1efd379d2702cd65
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-29 10:01:19 -0600

    Agregando ejemplo de test con docstring

--- algoritmos.py
+++ algoritmos.py
@@ -12,3 +12,21 @@ def fibonacci(number):
         return 1
     else: 
         return fibonacci(number-1) + fibonacci(number-2)
+
+
+def palindromo(sentence):
+    """ Retorno verdadero si la sentencia es palindromo
+    en caso contrario retorna falso
+
+    sentence -- String o entero
+
+    >>> palindromo("anita lava la tina")
+    True
+    >>> palindromo(12321)
+    True
+    >>> palindromo("test")
+    False
+    """
+
+    sentence = str(sentence).lower().replace(" ", "")
+    return sentence == sentence[::-1]
